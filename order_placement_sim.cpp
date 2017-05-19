0f6988c8ef5b2d4e9b8b3824710a64e28b5f1891
Abishek <abishek.v.a@gmail.com>
2017-05-19 23:02:28 +0200

    Update order_placement_sim.cpp

--- order_placement_sim.cpp
+++ order_placement_sim.cpp
@@ -1,4 +1,4 @@
-* Simulator for firing orders,takes client id ,client and scrip name and fires the order for multiple persons at a time. 
+/* Simulator for firing orders,takes client id ,client and scrip name and fires the order for multiple persons at a time. 
 	Though certain functionalities can be extended here it is not included for easy analysing.....meme is pronounced as miim.
 */
 #include <iostream>
